3feb8c4d7c182580985dedcafd9b7870744d314f
qqgameover <kasper35@live.no>
2021-08-30 13:49:12 +0200

    Update model.js

--- model.js
+++ model.js
@@ -1,6 +1,6 @@
 const model = {
 	app: {
-		currentPage: null,
+		currentPage: "meldinger",
 		currentUser: null,
 		currentTaskId: 2,
 
@@ -13,16 +13,17 @@ const model = {
 		taskNodes: [
 			{ id: 1, name: 'Fysisk', parent: null, videoUrl: '' },
 			{ id: 2, name: 'Sit-ups', parent: 1, videoUrl: '' },
-			{ id: 3, name: '5 sit-ups', parent: 2, videoUrl: '', points: 1 },
-			{ id: 4, name: '10 sit-ups', parent: 2, videoUrl: '', points: 1 },
-			{ id: 5, name: '20 sit-ups', parent: 2, videoUrl: '', points: 1 },
+			{ id: 3, name: 'Mental-styrke', parent: 1, videoUrl: '' },
+			{ id: 4, name: '5 sit-ups', parent: 2, videoUrl: '', points: 1 },
+			{ id: 5, name: '10 sit-ups', parent: 2, videoUrl: '', points: 1 },
+			{ id: 6, name: '20 sit-ups', parent: 2, videoUrl: '', points: 1 },
 		],
 		statistikk: {
 			antallMeldinger: 1,
 			sendteMeldinger: [{
 				avsender: "Kasper",
 				mottaker: "Olga",
-				innhold: "Jeg elsker deg ikke lenger!!!!1!!!2!!!",
+				innhold: "",
 			}],
 			instanser: [
 
@@ -39,14 +40,14 @@ const model = {
 					dob: 100699,
 					tilhoerlighet: "Mellomhagen ungdomsskole",
 					opptjentePoeng: 3006,
-					epost: "epost"
+					epost: "kasper35@live.no"
 				},
 				{
 					navn: "Jostein Gjessing Bay",
 					dob: 110500,
 					tilhoerlighet: "Bø Ungdomsskule",
 					opptjentePoeng: 12,
-					epost: "epost"
+					epost: "Jogba@live.no"
 				}
 			]
 
@@ -55,36 +56,66 @@ const model = {
 			{ text: "du er tatt på fersken i å", ledd: 0 },
 			{ text: "være støttende", ledd: 1 },
 			{ text: "hjelpe andre", ledd: 1 },
+			{ text: "invitere andre med", ledd: 1 },
+			{ text: "spre glede", ledd: 1 },
+			{ text: "lage god stemning", ledd: 1 },
+			{ text: "bli kjent med nye", ledd: 1 },
+			{ text: "spre god energi", ledd: 1 },
+			{ text: "heie på andre", ledd: 1 },
+			{ text: "være modig", ledd: 1 },
+			{ text: "være modig", ledd: 1 },
+			{ text: "utgjøre en positiv forskjell", ledd: 1 },
+			{ text: "lytte", ledd: 1 },
+			{ text: "utfordre deg selv", ledd: 1 },
+			{ text: "jobbe målrettet", ledd: 1 },
+			{ text: "prøve noe nytt", ledd: 1 },
+			{ text: "Se nye muligheter", ledd: 1 },
+			{ text: "gi kompliment", ledd: 1 },
+			{ text: "være raus", ledd: 1 },
+			{ text: "samarbeide godt", ledd: 1 },
+			{ text: "ta ansvar", ledd: 1 },
+			{ text: "oppmuntre", ledd: 1 },
+			{ text: "være tålmodig", ledd: 1 },
+			{ text: "vise engasjement", ledd: 1 },
+			{ text: "vise respekt", ledd: 1 },
+			{ text: "lære bort noe du er god på", ledd: 1 },
+			{ text: "være nysgjerrig", ledd: 1 },
+			{ text: "være lojal", ledd: 1 },
+			{ text: "inkludere andre", ledd: 1 },
+			{ text: "invitere andre med", ledd: 1 },
+
+			{ text: "stå på", ledd: 2 },
+			{ text: "fortsette akkurat sånn", ledd: 2 },
+			{ text: "fortsatt være den flotte personen du er", ledd: 2 },
+			{ text: "stå på videre", ledd: 2 },
+			{ text: "gjerne gjøre det igjen", ledd: 2 },
+			{ text: "gjenta suksessen", ledd: 2 },
+			{ text: "ikke slutte med det", ledd: 2 },
+			{ text: "ikke gi deg", ledd: 2 },
+			{ text: "utforske enda mer", ledd: 2 },
+
+			{ text: "du er fantastisk", ledd: 3 },
+			{ text: "du er en god venn", ledd: 3 },
+			{ text: "du motiverer", ledd: 3 },
+			{ text: "du gjør meg glad", ledd: 3 },
+			{ text: "du inspirerer", ledd: 3 },
+			{ text: "du skaper trygghet", ledd: 3 },
+			{ text: "du er en stjerne", ledd: 3 },
+			{ text: "du går foran som et godt eksempel", ledd: 3 },
+			{ text: "du er helt rå", ledd: 3 },
+			{ text: "du er lett å like", ledd: 3 },
+			{ text: "du er en nydelig person", ledd: 3 },
 		],
 
 		meldinger: {
-			ledd0: ["du er tatt på fersken i å"],
-			ledd1: ["være støttende", "hjelpe andre", "invitere andre med", "bli kjent med nye", "inkludere andre",
-				"spre glede", "lage god stemning", "spre god energi", "heie på andre", "være modig", "utfordre deg selv",
-				"utgjøre en positiv forskjell", "jobbe målrettet", "prøve noe nytt", "gi kompliment", "Se nye muligheter",
-				"lytte", "ta ansvar", "være raus", "samarbeide godt", "være tålmodig", "vise respekt", "være nysgjerrig",
-				"oppmuntre", "vise engasjement", "være lojal", "lære bort noe du er god på",
-			],
-			ledd2: ["stå på", "fortsette akkurat sånn", "fortsatt være den flotte personen du er", "stå på videre",
-				"gjerne gjøre det igjen", "ikke slutte med det", "ikke gi deg", "utforske enda mer",
-				"gjenta suksessen"],
-			ledd3: ["du er fantastisk", "du er en god venn", "du inspirerer", "du motiverer", "du gjør meg glad",
-				"du skaper trygghet", "du er en stjerne", "du er helt rå", "du er lett å like",
-				"du går foran som et godt eksempel", "du er en nydelig person",],
-			inbox: {
-				dineMeldinger: [
-					{ melding: "du er stygg og teit, ingen liker deg og du kommer til å dø alene haha" },
-				]
-			},
 			utbox: {
 				dineSendteMeldinger: [
-					{ melding: "Jeg synes du er rar, og jeg tror du er adoptert" }
+					{ melding: "" }
 				]
 			},
 		},
 	}
 }
-
 
 
 console.log(model.data.statistikk.folkOgFe)
