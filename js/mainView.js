beaac4979392365297cec58a490526d4284352a2
qqgameover <kasper35@live.no>
2021-08-30 14:44:41 +0200

    Create mainView.js

---
+++ js/mainView.js
@@ -0,0 +1,17 @@
+
+// Mailinn
+
+function updateViewMain() {
+	let html = `
+	
+	<div class="grid-containerMain">
+		<div class="grid-itemMain">Mental Styrke</div>
+		<div class="grid-itemMain">Fysisk Aktivitet</div>
+		<div class="grid-itemMain">Meldinger</div>
+		<div class="grid-itemMain">Lagånd</div>
+	</div>
+
+`
+	return html;
+	;
+}
