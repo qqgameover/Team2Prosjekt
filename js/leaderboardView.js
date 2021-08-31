a7721fecc40967b7d347fbcfa63ebfe9d5c1bbeb
qqgameover <kasper35@live.no>
2021-08-31 14:42:00 +0200

    Update leaderboardView.js

--- js/leaderboardView.js
+++ js/leaderboardView.js
@@ -1,9 +1,8 @@
 //Mailinn
 
-//Beste skole
-//Beste klasse
-//Beste lag
-
+let rankingSkole = [];
+let rankingKlasse = [];
+let rankingIndivid = [];
 function updateViewLeaderboard() {
 	html = `
 	<div class="leaderboard-grid">        
@@ -23,16 +22,34 @@ function updateViewLeaderboard() {
 
 		<table class="leaderboardTable">
 			<tr class="trTable">
-				<th class="thTable">Lag</th>
+				<th class="thTable">Elev i din klasse</th>
 				<th class="thTable">Poeng</th>
 			</tr>
 		</table>
 	</div>	
     
 		`;
-	for (let i = 0; i < model.data.statistikk.length; i++) {
-		html += createTableRow(i)
-			;
+	for (let i = 0; i < model.data.statistikk.instanser.length; i++) {
+		if (!model.data.statistikk.instanser[i].parent) {
+			rankingSkole.push(model.data.statistikk.instanser[i])
+			const sortedArray = rankingSkole.sort((a, b) => {
+				if (a.points < b.points) return 1;
+				return -1;
+			});
+			console.log(sortedArray)
+			rankingSkole = sortedArray;
+		}
+		if (model.app.currentUser.parent == model.data.statistikk.instanser[i].id) {
+			rankingKlasse.push(model.data.statistikk.instanser[i]);
+			const sortedArray = rankingKlasse.sort((a, b) => {
+				if (a.points < b.points) return 1;
+				return -1;
+			})
+			console.log(sortedArray);
+			rankingKlasse = sortedArray;
+		}
+		//html += createTableRow(i)
+		//;
 	}
 	html += `
 
@@ -45,29 +62,30 @@ function updateViewLeaderboard() {
 
 function createTableRow(i) {
 	const topgroup = model.data.statistikk.instanser[i]; //Beste skole
-	const subgroup = model.data.statistikk.instanser[i]; //Beste klasse
+	//Beste klasse i din skole	
+	const subgroup = model.data.statistikk.instanser[i]; //Beste elev i din klasse
 	const points = model.data.statistikk.instanser[i]; //Poeng
 
 	return `
 	<div class="leaderboard-grid">
 		<table class="leaderboardTable">
 			<tr class="trTable">
-				<td class="tdLeaderboard"></td>
-				<td class="tdLeaderboard">${points.anntallPoeng}</td>
+				<td class="tdLeaderboard">${rankingArray[0].skole}</td>
+				<td class="tdLeaderboard">${rankingArray[0].points}</td>
 			</tr>
 		</table>
 
 		<table class="leaderboardTable">
 			<tr class="trTable">
-				<td class="tdLeaderboard"></td>
-				<td class="tdLeaderboard">${points.anntallPoeng}</td>
+				<td class="tdLeaderboard">${rankingArray[1].klasse}</td>
+				<td class="tdLeaderboard">${rankingArray[0].points}</td>
 			</tr>	
 		</table>
 
 		<table class="leaderboardTable">
 			<tr class="trTable">
 				<td class="tdLeaderboard"></td>
-				<td class="tdLeaderboard">${points.anntallPoeng}</td>
+				<td class="tdLeaderboard"></td>
 			</tr>	
 		</table>
 	</div>
