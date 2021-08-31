1d341473133dd6d8713f6aa65cc54ba2ce35c3bd
qqgameover <kasper35@live.no>
2021-08-31 11:34:25 +0200

    Update meldingView.js

--- js/meldingView.js
+++ js/meldingView.js
@@ -4,7 +4,7 @@ function updateViewMeldinger() {
 	let ledd1 = [];
 	let ledd2 = [];
 	let ledd3 = [];
-	const ledd = function () {
+	const ledd = () => {
 		for (let i = 0; i < model.data.muligeMeldinger.length; i++) {
 			if (model.data.muligeMeldinger[i].ledd == 0) {
 				ledd0.push(model.data.muligeMeldinger[i]);
@@ -21,12 +21,30 @@ function updateViewMeldinger() {
 		}
 	}
 	ledd();
-	html = `<div id="container"> 
-	<h1>Send melding til noen!</h1>
-	<select id="ledd0Select" multiple>`
+	html = `<div class="containerMessages"> 
+	<lable for="messageSearch">Send melding til noen!</lable>
+	<input type="search" id="messageSearch"/>
+	<select id="ledd0Select">`
 	for (let i = 0; i < ledd0.length; i++) {
 		html += `<option value="${ledd0[i].text}">${ledd0[i].text}</option>`
 	}
-	html += `</select>`
+	html += `</select>
+	<select id="ledd1Select">`
+	for (let i = 0; i < ledd1.length; i++) {
+		html += `<option value="${ledd1[i].text}">${ledd1[i].text}</option>`
+	}
+	html += `</select>
+	<select id="ledd2Select">`
+	for (let i = 0; i < ledd2.length; i++) {
+		html += `<option value="${ledd2[i].text}">${ledd2[i].text}</option>`
+	}
+	html += `</select>
+	<select id="ledd2Select">`
+	for (let i = 0; i < ledd3.length; i++) {
+		html += `<option value="${ledd3[i].text}">${ledd3[i].text}</option>`
+	}
+	html += `</select> 
+	<button onclick="sendMessage()">Send melding</button>
+	</div>`
 	return html;
 }
