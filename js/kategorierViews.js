dbdb9f7a6fac1dbf60b82f1d54d9cee6d95cfb81
qqgameover <kasper35@live.no>
2021-08-31 11:34:14 +0200

    Update kategorierViews.js

--- js/kategorierViews.js
+++ js/kategorierViews.js
@@ -1,6 +1,14 @@
 function kategoriView(page) {
-	let html = ""
-	html = `<div id="video" href=""></div>
-            
+	let html = "";
+	let tasks = [];
+	const taskManager = () => {
+		for (let i = 0; x; i++) {
+
+		}
+	}
+	html = `<div id="video" href=${tasks}></div>
+			<div id=1></div>
+			<div id=2></div></div>
+			<div id=3></div>
     `
 }
