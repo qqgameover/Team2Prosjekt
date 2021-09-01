a1796b6d02cdb7d99e9e19bdec39db194d6a2596
qqgameover <kasper35@live.no>
2021-09-01 14:57:35 +0200

    Update meldingView.js

--- js/meldingView.js
+++ js/meldingView.js
@@ -27,15 +27,18 @@ function updateViewMeldinger() {
 		}
 	}
 	ledd();
-	html = `<div class="containerMessages"> 
+	html = `
+	<div id="top" class="containerMessages">
 	<lable for="messageSearch">Send melding til noen!</lable>
 	<input type="search" id="messageSearch"/>
-	<select onchange="ledd0Valg = this.value" id="ledd0Select">`
+	</div>
+	<div class="containerMessages"> 
+	<select id="select0" onchange="ledd0Valg = this.value" id="ledd0Select">`
 	for (let i = 0; i < ledd0.length; i++) {
 		html += `<option value="${ledd0[i].text}">${ledd0[i].text}</option>`
 	}
 	html += `</select>
-	<select onchange="ledd1Valg = this.value; updateView()" id="ledd1Select">`
+	<select id="select1" onchange="ledd1Valg = this.value; updateView()" id="ledd1Select" multiple>`
 	for (let i = 0; i < ledd1.length; i++) {
 		if (ledd1Valg == ledd1[i].text) {
 			html += `<option selected value="${ledd1[i].text}">${ledd1[i].text}</option>`
@@ -44,7 +47,7 @@ function updateViewMeldinger() {
 		}
 	}
 	html += `</select>
-	<select onchange="ledd2Valg = this.value; updateView()" id="ledd2Select">`
+	<select id="select2" onchange="ledd2Valg = this.value; updateView()" id="ledd2Select" multiple>`
 	for (let i = 0; i < ledd2.length; i++) {
 		if (ledd2Valg == ledd2[i].text) {
 			html += `<option selected value="${ledd2[i].text}">${ledd2[i].text}</option>`
@@ -53,7 +56,7 @@ function updateViewMeldinger() {
 		}
 	}
 	html += `</select>
-	<select onchange="ledd3Valg = this.value; updateView()" id="ledd2Select">`
+	<select id="select3" onchange="ledd3Valg = this.value; updateView()" id="ledd2Select" multiple>`
 	for (let i = 0; i < ledd3.length; i++) {
 		if (ledd3Valg == ledd3[i].text) {
 			html += `<option selected value="${ledd3[i].text}">${ledd3[i].text}</option>`
@@ -63,11 +66,13 @@ function updateViewMeldinger() {
 	}
 	html += `</select> 
 	</div>
-	<div class="containerForFluff">
-	<p id="valgt melding">${ledd0Valg || "Du er tatt på fersken i"} ${ledd1Valg || "..."} ${ledd2Valg || "..."} ${ledd3Valg || "..."}</p>
 	</div>
 	<div class="containerForFluff">
-	<button onclick="sendMessage()">Send melding</button>
+	<p id="valgt melding">${ledd0Valg || "Du er tatt på fersken i"} ${ledd1Valg || "..."} ${ledd2Valg || "..."} ${ledd3Valg || "..."}</p>
+	<button class="sendButton" onclick="sendMessage()">Send melding</button>
+	</div>
+	<div class="navbar">
+		<a href="#Hjem" onclick="gotoH()">Hjem</a>
 	</div>`
 	return html;
 }
