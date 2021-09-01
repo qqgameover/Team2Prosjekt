a4af0b7c3f7f8d9170f10f758e226b93ab41a950
qqgameover <kasper35@live.no>
2021-09-01 14:57:30 +0200

    Update kategorierViews.js

--- js/kategorierViews.js
+++ js/kategorierViews.js
@@ -1,18 +1,34 @@
 // Jostein
+pageid = 1;
 function updateViewKategorier(pageid) {
-	let html = `<div id="video"></div>`;
+	let html = `<iframe width="1024" height="730" src="https://www.youtube.com/embed/JB-G_o9WB9E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 	let tasks = [];
 	const taskManager = (pageid) => {
 		for (let i = 0; i < model.data.taskNodes.length; i++) {
 			if (model.data.taskNodes[i].parent == pageid) {
-				console.log("inni if");
 				tasks.push(model.data.taskNodes[i]);
+				console.log(tasks)
 			}
 		}
 		for (let i = 0; i < tasks.length; i++) {
-			html += `<div id=${tasks[i].id} onclick="addPoints()">${tasks[i].name}</div>`
+			html += `<div class=katBtn onclick="pageid(${tasks[i].id})">${tasks[i].name}</div>`
+
 		}
 	}
 	taskManager(pageid);
+	html +=
+		`<div class="navbar">
+		<a href="#Hjem" onclick="gotoH()">Hjem</a>
+	</div>`
+
 	return html;
+	function pageid(id) {
+		if (pageid < 14) {
+			pageid = id
+		}
+		else {
+			addpoints()
+		}
+		updateView();
+	}
 }
