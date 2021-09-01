8ad62d382d02a6fbef0b815f4703a8a538186de1
qqgameover <kasper35@live.no>
2021-09-01 14:57:34 +0200

    Update mainView.js

--- js/mainView.js
+++ js/mainView.js
@@ -4,25 +4,21 @@ function updateViewMain() {
 	let html = `
 
 
-	<div id="mainView">
-	  <div class="grid-container">
-		  <div class="grid-item">Mental Styrke</div>
-		  <div class="grid-item">Fysisk Aktivitet</div>
-		  <div class="grid-item">Meldinger</div>  
-		  <div class="grid-item">Lagånd</div>
-	  </div>
-  </div>
+<div class="mainView">
+	<div class="grid-containerTop">
+		<div class="grid-item" onclick="gotoKategorier()"; "pageid(3)">Mental Styrke </div>
+		<div class="grid-item" onclick="gotoKategorier()"; "pageid(2)">Fysisk Aktivitet </div>
+		<div class="grid-item" onclick="gotoKategorier()"; "pageid(4)"> Lagånd </div>
+	</div>
+	<div class="grid-containerBot">	
+		<div class="grid-item" onclick="gotoMeldinger()">Meldinger </div>
+		<div class="grid-item" onclick="gotoLederbrett()">Lederbrett </div> 
+	</div>
+</div>
 
 `
 
 	return html;
 }
 
-/* <div class="border">
-  <div class="grid">
-    <div>Item 1</div>
-    <div>Item 2</div>
-    <div>Item 3</div>
-    <div>Item 4</div>
-  </div>
-</div> */
+
