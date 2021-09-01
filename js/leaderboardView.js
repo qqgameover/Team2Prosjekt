06a4232378b6cbff0e7fbf9704903242f73e8f30
qqgameover <kasper35@live.no>
2021-09-01 14:57:32 +0200

    Update leaderboardView.js

--- js/leaderboardView.js
+++ js/leaderboardView.js
@@ -3,7 +3,11 @@
 let rankingSkole = [];
 let rankingKlasse = [];
 let rankingIndivid = [];
+let skoleRankingRows;
 function updateViewLeaderboard() {
+	rankingSkole = [];
+	rankingKlasse = [];
+	rankingIndivid = [];
 	let html = "";
 	html = `
 	<div class="leaderboard-grid">        
@@ -28,6 +32,10 @@ function updateViewLeaderboard() {
 			</tr>
 		</table>
 	</div>	
+
+	<div class="navbar">
+		<a href="#Hjem" onclick="gotoH()">Hjem</a>
+	</div>
     
 		`;
 	for (let i = 0; i < model.data.statistikk.instanser.length; i++) {
@@ -37,49 +45,30 @@ function updateViewLeaderboard() {
 				if (a.points < b.points) return 1;
 				return -1;
 			});
-			console.log(sortedArray)
+			console.log(sortedArray, 'Egget')
 			rankingSkole = sortedArray;
 		}
-		// if (model.app.currentUser.parent == model.data.statistikk.instanser[i].id) {
-		// 	rankingKlasse.push(model.data.statistikk.instanser[i]);
-		// 	const sortedArray = rankingKlasse.sort((a, b) => {
-		// 		if (a.points < b.points) return 1;
-		// 		return -1;
-		// 	})
-		// 	console.log(sortedArray);
-		// 	rankingKlasse = sortedArray;
-		// }
-		//html += createTableRow(i)
-		//;
 	}
-	html += `
-
-	<div class="footer"> 
-	</div>
-
-    `
+	html += skoleRanking();
 	return html;
 }
 
-function createTableRow(i) {
-	const topgroup = model.data.statistikk.instanser[i]; //Beste skole
+function createTableRow(rankingParam) {
+	// const topgroup = model.data.statistikk.instanser[i]; //Beste skole
 	//Beste klasse i din skole	
-	const subgroup = model.data.statistikk.instanser[i]; //Beste elev i din klasse
-	const points = model.data.statistikk.instanser[i]; //Poeng
+	// const subgroup = model.data.statistikk.instanser[i]; //Beste elev i din klasse
+	// const points = model.data.statistikk.instanser[i]; //Poeng
 
 	return `
 	<div class="leaderboard-grid">
 		<table class="leaderboardTable">
-			<tr class="trTable">
-				<td class="tdLeaderboard">${rankingArray[0].skole}</td>
-				<td class="tdLeaderboard">${rankingArray[0].points}</td>
-			</tr>
+				${rankingParam}
 		</table>
 
 		<table class="leaderboardTable">
 			<tr class="trTable">
-				<td class="tdLeaderboard">${rankingArray[1].klasse}</td>
-				<td class="tdLeaderboard">${rankingArray[0].points}</td>
+				<td class="tdLeaderboard">${""}</td>
+				<td class="tdLeaderboard">${""}</td>
 			</tr>	
 		</table>
 
@@ -92,3 +81,12 @@ function createTableRow(i) {
 	</div>
 `;
 }
+function skoleRanking() {
+	skoleRankingRows = "";
+	for (let i = 0; i < rankingSkole.length; i++) {
+		skoleRankingRows += `<tr class="trTable"><td class="tdLeaderboard">${rankingSkole[i].skole}</td></tr>
+		<tr class="trTable"><td class="tdLeaderboard">${rankingSkole[i].points}</td></tr>`;
+	}
+	return createTableRow(skoleRankingRows);
+}
+
