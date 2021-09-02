36ace19f750548da041c410862e4d9704b8e2df4
qqgameover <kasper35@live.no>
2021-09-02 12:13:50 +0200

    Update mainView.js

--- js/mainView.js
+++ js/mainView.js
@@ -5,9 +5,9 @@ function updateViewMain() {
 
 <div class="mainGrid">
 	<div class="grid-item" onclick="gotoMeldinger()">Meldinger </div>
-	<div class="grid-item" onclick="gotoKategorier()"; "pageid(3)">Mental Styrke </div>
-	<div class="grid-item" onclick="gotoKategorier()"; "pageid(2)">Fysisk Aktivitet </div>
-	<div class="grid-item" onclick="gotoKategorier()"; "pageid(4)"> Lagånd </div>	
+	<div class="grid-item" onclick="gotoKategorier(3)">Mental Styrke </div>
+	<div class="grid-item" onclick="gotoKategorier(2)">Fysisk Aktivitet </div>
+	<div class="grid-item" onclick="gotoKategorier(4)"> Lagånd </div>	
 </div>		
 	
 	<div class="navbar">
