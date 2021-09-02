3dc5252d30feebd13e6cf7ec13c00049b9702a03
qqgameover <kasper35@live.no>
2021-09-02 12:13:46 +0200

    Update kategorierViews.js

--- js/kategorierViews.js
+++ js/kategorierViews.js
@@ -1,51 +1,56 @@
 // Jostein
-pageid = 1;
-function updateViewKategorier(pageid) {
+let pageid = "";
+function updateViewKategorier() {
 	let html = `<div class="parent">
 						<div class="visual">
 							<iframe 
-							id="video"
-							src="https://www.youtube.com/embed/JB-G_o9WB9E" 
-							title="YouTube video player" frameborder="0" 
-							allow="accelerometer; autoplay; clipboard-write; 
-							encrypted-media; gyroscope; picture-in-picture" 
-							allowfullscreen></iframe>
-						</div>	
-				</div>	
+								id="video"
+								src="https://www.youtube.com/embed/JB-G_o9WB9E" 
+								title="YouTube video player" frameborder="0" 
+								allow="accelerometer; autoplay; clipboard-write; 
+								encrypted-media; gyroscope; picture-in-picture" 
+								allowfullscreen>
+							</iframe>
+						<div id="kategoriknapper">
+					
 	`;
 
 	let tasks = [];
-	const taskManager = (pageid) => {
+	const taskManager = () => {
 		for (let i = 0; i < model.data.taskNodes.length; i++) {
 			if (model.data.taskNodes[i].parent == pageid) {
 				tasks.push(model.data.taskNodes[i]);
-				console.log(tasks)
 			}
 		}
 		for (let i = 0; i < tasks.length; i++) {
-			html += `<div 	
-					class=katBtn 
-					onclick="pageid(${tasks[i].id})">
-					${tasks[i].name}
-					</div>
-				`
+			html += `<button 	
+						class="katBtn" 
+						onclick="pagemanager(${tasks[i].id})">
+						${tasks[i].name}
+					</button>`
 		}
 	}
 	taskManager(pageid);
 	html +=
-		`<div class="navbar">
-			<a href="#Lederbrett" onclick="gotoLederbrett()">Lederbrett</a>
-			<a href="#Hjem" onclick="gotoH()">Hjem</a>
-		</div>`
+		`</div>
+		</div>
+			</div>
+	<div class="navbar">
+		<a href="#Lederbrett" onclick="gotoLederbrett()">Lederbrett</a>
+		<a href="#Hjem" onclick="gotoH()">Hjem</a>
+	</div>`
+
 
 	return html;
-	function pageid(id) {
-		if (pageid < 14) {
-			pageid = id
-		}
-		else {
-			addpoints()
-		}
-		updateView();
+
+}
+function pagemanager(id) {
+	if (id < 14) {
+		pageid = id;
+
 	}
+	else {
+		addpoints();
+	}
+	updateView();
 }
