246f06aea50ed2593db20b1e2c0c224d00520814
qqgameover <kasper35@live.no>
2021-09-03 14:38:24 +0200

    Create meldingController.js

---
+++ meldingController.js
@@ -0,0 +1,37 @@
+function searchName(searchVal) {
+	const search = model.data.statistikk.instanser.map(function (α) {
+		return { navn: α.navn, userName: α.userName };
+	});
+	const findPerson = search.find(function (α) {
+		if (searchVal == α.navn) {
+			return α;
+		} else {
+			return false;
+		}
+	})
+	if (!findPerson) {
+		console.log("WOW")
+		return;
+	}
+	return findPerson.userName
+}
+
+function sendMessage(ledd0, ledd1, ledd2, ledd3, motakker, avsender) {
+	model.data.statistikk.antallMeldinger++;
+	model.data.statistikk.sendteMeldinger.push(
+		{
+			avsender: avsender,
+			mottaker: motakker,
+			ledd0: ledd0,
+			ledd1: ledd1,
+			ledd2: ledd2,
+			ledd3: ledd3,
+		});
+	model.data.statistikk.achievements.push(
+		{
+			user: avsender,
+			data: new Date(),
+			task: 0,
+			pointsEarned: 1,
+		});
+}
