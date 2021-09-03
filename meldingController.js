e410eac012312f258ed6c70a40555d729c86179a
qqgameover <kasper35@live.no>
2021-09-03 20:55:41 +0200

    Update meldingController.js

--- meldingController.js
+++ meldingController.js
@@ -1,23 +1,49 @@
 //Kasper B)
 
 function searchName(searchVal) {
+	const splitNameArray = [];
+	const suggestedNamesArray = [];
 	const search = model.data.statistikk.instanser.map(function (α) {
 		return { navn: α.navn, userName: α.userName };
 	});
-	const findPerson = search.find(function (α) {
-		if (searchVal == α.navn) {
-			return α;
-		} else {
-			return false;
-		}
+	function splitNameArrayFunc() {
+		search.forEach((person) => {
+			if (!person.navn) return;
+			splitNameArray.push(person.navn.split(""));
+		})
+	}
+	splitNameArrayFunc();
+	//Vet ikke hvorfor, men dette funker jo fett.
+	//Ikke rør det plz, det er turbo cursed, mvh Kasper
+	const currentSearchValue = searchVal.split("");
+	const findPerson = splitNameArray.forEach((name, index) => {
+		name.forEach((char, index) => {
+			if (currentSearchValue[index] == char) {
+				if (!suggestedNamesArray.includes(name)) {
+					suggestedNamesArray.push(name);
+				}
+			} else {
+				return false;
+			}
+		})
+	})
+	const returnToArray = [];
+	const returnToArrayFunc = suggestedNamesArray.forEach((name, index) => {
+		returnToArray.push(name.join(""))
 	})
 	if (!findPerson) {
 		return;
+	} else {
+		return returnToArray;
 	}
-	return findPerson.userName
 }
 
+
 function sendMessage(ledd0, ledd1, ledd2, ledd3, motakker, avsender) {
+	if (!motakker) {
+		alert("Ingen mottaker funnet");
+		return;
+	}
 	model.data.statistikk.antallMeldinger++;
 	model.data.statistikk.sendteMeldinger.push(
 		{
