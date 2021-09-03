17f9ec2c8bdc51ea851c45e8bf61dba3bea2b881
qqgameover <kasper35@live.no>
2021-09-03 14:38:27 +0200

    Update meldingView.js

--- meldingView.js
+++ meldingView.js
@@ -1,75 +1,56 @@
 //Kasper :)
 
-let ledd0Valg;
-let ledd1Valg;
-let ledd2Valg;
-let ledd3Valg;
+let leddValg = [[], [], [], []];
+let mottaker = "";
+
 function updateViewMeldinger() {
 	let html = "";
-	let ledd0 = [];
-	let ledd1 = [];
-	let ledd2 = [];
-	let ledd3 = [];
+	let leddArray = [[], [], [], []];
 	const ledd = () => {
 		for (let i = 0; i < model.data.muligeMeldinger.length; i++) {
 			if (model.data.muligeMeldinger[i].ledd == 0) {
-				ledd0.push(model.data.muligeMeldinger[i]);
+				leddArray[0].push(model.data.muligeMeldinger[i]);
 			}
 			if (model.data.muligeMeldinger[i].ledd == 1) {
-				ledd1.push(model.data.muligeMeldinger[i]);
+				leddArray[1].push(model.data.muligeMeldinger[i]);
 			}
 			if (model.data.muligeMeldinger[i].ledd == 2) {
-				ledd2.push(model.data.muligeMeldinger[i]);
+				leddArray[2].push(model.data.muligeMeldinger[i]);
 			}
 			if (model.data.muligeMeldinger[i].ledd == 3) {
-				ledd3.push(model.data.muligeMeldinger[i]);
+				leddArray[3].push(model.data.muligeMeldinger[i])
 			}
 		}
 	}
 	ledd();
 	html = `
 	<div id="top" class="containerMessages">
-	<lable for="messageSearch">Send melding til noen!</lable>
-	<input type="search" id="messageSearch"/>
+		<lable for="messageSearch">Send melding til noen!</lable>
+		<input oninput="searchName(this.value) ; mottaker = this.value" type="text" id="messageSearch" value = "${mottaker}"/>
 	</div>
-	<div class="containerMessages"> 
-	<select id="select0" onchange="ledd0Valg = this.value" id="ledd0Select">`
-	for (let i = 0; i < ledd0.length; i++) {
-		html += `<option value="${ledd0[i].text}">${ledd0[i].text}</option>`
-	}
-	html += `</select>
-	<select id="select1" onchange="ledd1Valg = this.value; updateView()" id="ledd1Select" multiple>`
-	for (let i = 0; i < ledd1.length; i++) {
-		if (ledd1Valg == ledd1[i].text) {
-			html += `<option selected value="${ledd1[i].text}">${ledd1[i].text}</option>`
-		} else {
-			html += `<option value="${ledd1[i].text}">${ledd1[i].text}</option>`
+	<div class="containerMessages"> `
+
+	for (let j = 0; j < 4; j++) {
+		html += `<select id="select${j}" onchange="leddValg[${j}] = this.value; updateView()" id="leddValg[${j}]" multiple>`
+		for (let i = 0; i < leddArray[j].length; i++) {
+			if (leddValg[j] == leddArray[j][i].text) {
+				html += `<option selected value="${leddArray[j][i].text}">${leddArray[j][i].text}</option>`
+			} else {
+				html += `<option value="${leddArray[j][i].text}">${leddArray[j][i].text}</option>`
+			}
 		}
+		html += `</select>`
 	}
-	html += `</select>
-	<select id="select2" onchange="ledd2Valg = this.value; updateView()" id="ledd2Select" multiple>`
-	for (let i = 0; i < ledd2.length; i++) {
-		if (ledd2Valg == ledd2[i].text) {
-			html += `<option selected value="${ledd2[i].text}">${ledd2[i].text}</option>`
-		} else {
-			html += `<option value="${ledd2[i].text}">${ledd2[i].text}</option>`
-		}
-	}
-	html += `</select>
-	<select id="select3" onchange="ledd3Valg = this.value; updateView()" id="ledd2Select" multiple>`
-	for (let i = 0; i < ledd3.length; i++) {
-		if (ledd3Valg == ledd3[i].text) {
-			html += `<option selected value="${ledd3[i].text}">${ledd3[i].text}</option>`
-		} else {
-			html += `<option value="${ledd3[i].text}">${ledd3[i].text}</option>`
-		}
-	}
+
 	html += `</select> 
 	</div>
 	</div>
 	<div class="containerForFluff">
-	<p id="valgt melding">${ledd0Valg || "Du er tatt på fersken i"} ${ledd1Valg || "..."} ${ledd2Valg || "..."} ${ledd3Valg || "..."}</p>
-	<button class="sendButton" onclick="sendMessage()">Send melding</button>
+	<p id="valgt melding">${leddValg[0]} ${leddValg[1]} ${leddValg[2]} ${leddValg[3]}</p>
+	<button class="sendButton" 
+		onclick="sendMessage(leddValg[0], leddValg[1], leddValg[2], leddValg[3], mottaker, model.app.currentUser)">
+		Send melding
+	</button>
 	</div>
 	<div class="navbar">
 		<a href="#Lederbrett" onclick="gotoLederbrett()">Lederbrett</a>
