b59a7a86c3de2f0c5825ab71bb5b8646fd608b75
qqgameover <kasper35@live.no>
2021-09-04 17:10:20 +0200

    Update meldingController.js

--- meldingController.js
+++ meldingController.js
@@ -13,6 +13,7 @@ function searchName(searchVal) {
 		})
 	}
 	splitNameArrayFunc();
+
 	//Vet ikke hvorfor, men dette funker jo fett.
 	//Ikke rør det plz, det er turbo cursed, mvh Kasper
 	const currentSearchValue = searchVal.split("");
@@ -22,7 +23,8 @@ function searchName(searchVal) {
 				if (!suggestedNamesArray.includes(name)) {
 					suggestedNamesArray.push(name);
 				}
-			} else {
+			} if (suggestedNamesArray.length == 0) {
+				console.log("Den er tom")
 				return false;
 			}
 		})
@@ -31,10 +33,13 @@ function searchName(searchVal) {
 	const returnToArrayFunc = suggestedNamesArray.forEach((name, index) => {
 		returnToArray.push(name.join(""))
 	})
-	if (!findPerson) {
-		return;
-	} else {
+	console.log(suggestedNamesArray)
+	if (returnToArray.length > 0) {
+		console.log("fant en person")
 		return returnToArray;
+	} else {
+		console.log("false")
+		return;
 	}
 }
 
@@ -59,4 +64,22 @@ function sendMessage(ledd0, ledd1, ledd2, ledd3, motakker, avsender) {
 			task: 0,
 			pointsEarned: 1,
 		});
+
+}
+
+function findUserName(searchVal) {
+	const search = model.data.statistikk.instanser.map(function (α) {
+		return { navn: α.navn, userName: α.userName };
+	});
+	const findPerson = search.find((person) => {
+		if (person.navn = searchVal) {
+			return person.userName
+		} else {
+			return false;
+		}
+	})
+	if (!findPerson) {
+		return false;
+	}
+	else return findPerson.userName;
 }
