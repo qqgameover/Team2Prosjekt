001f2e98490a1aa5dbdc417abbdc7d0f94592b67
Jostein <61984675+AsthmaticDragon@users.noreply.github.com>
2021-09-17 19:01:27 +0200

    Add files via upload

--- CONTROLLER/videoController.js
+++ CONTROLLER/videoController.js
@@ -1,17 +1,9 @@
 function pagemanager(id, kategori, point) {
-	if (id == 567) {
-		html = updateViewLeaderboard();
-		document.getElementById("app").innerHTML = html;
-	}
-	else if (id == 1) {
-		html = updateViewMain();
-		document.getElementById("app").innerHTML = html;
-	}
-	else if (id < 14 && id != 1 && id != 567) {
+	if (id < 14) {
 		let pageToAdd = model.data.taskNodes[id - 1].parent;
 		model.app.pageId = id;
 		model.app.currentVideo = model.data.taskNodes[id - 1].videoUrl;
-		model.app.previousPage.push(`pagemanager(${pageToAdd})`);
+		model.app.previousPage.push({name: "kategorier", id: `${pageToAdd}`});
 		updateView();
 	}
 	else {
