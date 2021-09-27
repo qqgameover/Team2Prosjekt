92b94ce1479b0857ae7c0256a16205a3340cb143
qqgameover <kasper35@live.no>
2021-09-27 14:13:31 +0200

    so long code.

--- CONTROLLER/videoController.js
+++ CONTROLLER/videoController.js
@@ -1,3 +1,5 @@
+const { text } = require("stream/consumers");
+
 function previousPageAdder(id, kategori, point) {
 	if (id < 14) {
 		let pageToAdd = model.data.taskNodes[id - 1].parent;
@@ -13,6 +15,24 @@ function pagemanager(id, kategori, point) {
 		updateView();
 	}
 	else {
-		addAch(model.app.currentUser, kategori, point);
+		addFluff(kategori, point);
 	}
+}
+
+function addFluff(kategori, point) {
+	let desc = addDesc()
+	if (desc == null || desc == "") return
+	else {
+		addAch(model.app.currentUser, kategori, point, desc);
+		return;
+	}
+}
+
+function addDesc() {
+	let text = prompt("skriv en kort beskrivelse av hva du har gjort.");
+	if (text == null || text == "") {
+		alert("legg til en beskrivelse for å få poeng");
+		return false
+	}
+	return text;
 }
