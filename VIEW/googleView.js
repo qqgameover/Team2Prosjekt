f0d57977ea48bf42007ba1dd8524e582a598eca7
qqgameover <kasper35@live.no>
2022-01-20 19:15:47 +0100

    Update googleView.js

--- VIEW/googleView.js
+++ VIEW/googleView.js
@@ -3,7 +3,7 @@ function googleView() {
 	let html = ""
 	html = `
 	<div class="googleStuff">
-	    <button class="btn btn-success" onclick="googSignIn()">Logg på</button>
+	    <button class="btn btn-lg btn-success" onclick="googSignIn()">Logg på</button>
     	</div>    
 	`
 	return html;
