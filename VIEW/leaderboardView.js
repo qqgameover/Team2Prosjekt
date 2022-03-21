ff6929a1668861969b956eb1e751a65ef00389bb
Kasper L <kasper35@live.no>
2022-03-21 13:28:03 +0100

    Update leaderboardView.js

--- VIEW/leaderboardView.js
+++ VIEW/leaderboardView.js
@@ -113,7 +113,7 @@ function skoleRanking() {
 	for (let i = 0; i < rankingKlasse.length; i++) {
 		klasseRankingRows +=
 			`<tr>
-				<td class="klasse">${rankingKlasse[i].klasse}</td>
+				<td class="klasse">${rankingKlasse[i].klasse} - ${rankingKlasse[i].skole}</td>
 				<td class="klassepoeng">${rankingKlasse[i].points}</td>
 			</tr>`;
 	}
