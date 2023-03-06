ae77c6054147f3bdde282180c7084e1416734e3a
Kasper <kasper35@live.no>
2023-03-06 23:48:40 +0100

    Nvm the fix didnt work

--- model.js
+++ model.js
@@ -238,7 +238,6 @@ const findClassParentWithName = (_class) => {
         .replace("Trinn 8", "")
         .replace("8trinn", "")
         .replace("5trinn", "")
-        .replace("Trinn larere")
         .replace("-", "")
         .replace(".csv", "")
         .replace("  ", " ")
