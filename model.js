d2cdd4c0637bd122d013e49119f94687a1ebf217
Kasper <kasper35@live.no>
2023-03-06 23:52:14 +0100

    ll

--- model.js
+++ model.js
@@ -238,6 +238,8 @@ const findClassParentWithName = (_class) => {
         .replace("Trinn 8", "")
         .replace("8trinn", "")
         .replace("5trinn", "")
+        .replace("Trinn larere")
+        .replace("larer", "lærer")
         .replace("-", "")
         .replace(".csv", "")
         .replace("  ", " ")
